3061a562cd4158c1c38fc1b29e35039c6d663219
Surdu Nicolae <nicolae.d.surdu@gmail.com>
2015-05-24 14:48:40 +0300

    Fixed Bluez installer and added reboot cofirmation

--- bin/install_bluez.sh
+++ bin/install_bluez.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo apt-get install libusb-dev libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev
 
@@ -6,7 +6,7 @@ sudo mkdir /opt/bluetooth
 cd /opt/bluetooth
 
 sudo wget https://www.kernel.org/pub/linux/bluetooth/bluez-5.23.tar.gz
-sudo tar xvzf bluetooth/bluez-5.23.tar.gz
+sudo tar xvzf bluez-5.23.tar.gz
 
 sudo rm bluez-5.23.tar.gz
 
@@ -15,4 +15,11 @@ sudo ./configure --disable-systemd
 sudo make
 sudo make install
 
-sudo reboot
+while true; do
+    read -p "Do you wish to reboot now ? [y/n]:" yn
+    case $yn in
+        [Yy]* ) sudo reboot; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer y for 'yes' or n for 'no'.";;
+    esac
+done
